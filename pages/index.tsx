530f26198d1a42c8f029f956bcbe255ff66c8167
Alexander <josenavasiv@gmail.com>
2022-12-05 11:47:21 -0800

    Client Side Rendering Pagination

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,94 @@
-import { PostsQuery, PostsDocument, Post } from '../graphql/__generated__/graphql';
-import { addApolloState, initializeApolloClient } from '../lib/apollo';
-import { GetServerSidePropsContext } from 'next/types';
+// import { PostsQuery, PostsDocument, Post, usePostsQuery } from '../graphql/__generated__/graphql';
+// import { addApolloState, initializeApolloClient } from '../lib/apollo';
+// import { GetServerSidePropsContext } from 'next/types';
+import { usePostsQuery } from '../graphql/__generated__/graphql';
 import Layout from '../components/Layout';
 import Link from 'next/link';
+import { VStack, Box, Heading, Text, Flex, Button } from '@chakra-ui/react';
 
-export async function getServerSideProps(ctx: GetServerSidePropsContext) {
-	const apolloClient = initializeApolloClient(null, ctx);
-	const { data } = await apolloClient.query<PostsQuery>({
-		query: PostsDocument,
-		variables: {
-			limit: 5,
-		},
-	});
+// export async function getServerSideProps(ctx: GetServerSidePropsContext) {
+// 	const apolloClient = initializeApolloClient(null, ctx);
+// 	const { data } = await apolloClient.query<PostsQuery>({
+// 		query: PostsDocument,
+// 		variables: {
+// 			limit: 10,
+// 		},
+// 	});
 
-	return addApolloState(apolloClient, {
-		props: {
-			posts: data.posts?.posts,
-		},
-	});
-}
+// 	return addApolloState(apolloClient, {
+// 		props: {
+// 			posts: data.posts?.posts,
+// 		},
+// 	});
+// }
 
-interface HomeProps {
-	posts: Post[];
-}
+// interface HomeProps {
+// 	posts: Post[];
+// }
 
-export default function Home({ posts }: HomeProps) {
+export default function Home() {
+	const { data, error, loading, fetchMore } = usePostsQuery({ variables: { limit: 10 } });
+
+	console.log(data?.posts.posts);
+
+	if (!loading && !data) {
+		return (
+			<div>
+				<div>ERROR</div>
+				<div>{error?.message}</div>
+			</div>
+		);
+	}
+
 	return (
 		<Layout>
-			<Link style={{ color: 'blue', textDecorationLine: 'underline' }} href="/create-post">
-				Create Post
-			</Link>
-			<div>
-				{posts.map((p) => {
-					return <div key={p.id}>{p.title}</div>;
-				})}
-			</div>
+			<Flex>
+				<Heading>The Blog</Heading>
+				<Link
+					style={{ color: 'blue', textDecorationLine: 'underline', marginLeft: 'auto' }}
+					href="/create-post"
+				>
+					Create Post
+				</Link>
+			</Flex>
+			<br />
+			{!data && loading ? (
+				<div>loading...</div>
+			) : (
+				<VStack spacing={8}>
+					{data?.posts.posts.map((p) => {
+						return (
+							<Box key={p.id} p={5} shadow="md" borderWidth="1px" w={'full'}>
+								<Heading fontSize="xl">{p.title}</Heading>
+								<Text mt={4}>{p.text.slice(0, 50)}</Text>
+							</Box>
+						);
+					})}
+				</VStack>
+			)}
+			{data && data.posts.hasMore ? (
+				<Flex>
+					<Button
+						isLoading={loading}
+						m="auto"
+						my={8}
+						onClick={() => {
+							fetchMore({
+								variables: {
+									limit: 10,
+									cursor: parseInt(data.posts.posts[data.posts.posts.length - 1].id),
+								},
+							});
+						}}
+					>
+						Load More
+					</Button>
+				</Flex>
+			) : (
+				<Box my={8}></Box>
+			)}
 		</Layout>
 	);
 }
+
+// Need to research SSR (getServerSideProps) + Pagination
